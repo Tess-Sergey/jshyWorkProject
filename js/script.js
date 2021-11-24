1f7de9f876d7ac4809c865b1739406dbd47941c4
sergey <lifeglobephoenix@gmail.com>
2021-11-24 17:19:48 +0300

    retyped code from first practice

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 
-// const numberOfFilms = alert( "Сколько фильмов уже посмотрели?");
+const numberOfFilms = alert( "Сколько фильмов уже посмотрели?");
 
 
-// const personalMovieDB = {
-//     count :  numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     janres: [],
-//     privat: false
+const personalMovieDB = {
+    count :  numberOfFilms,
+    movies: {},
+    actors: {},
+    janres: [],
+    privat: false
 
-// };
+};
 
-// const a = prompt("Один из последних фильмов");
-// const b = prompt("На сколько оценитие его");
-// const с = prompt("Один из последних фильмов");
-// const d  = prompt("На сколько оценитие его");
+const a = prompt("Один из последних фильмов");
+const b = prompt("На сколько оценитие его");
+const с = prompt("Один из последних фильмов");
+const d  = prompt("На сколько оценитие его");
 
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[b] = d;
-// console.log(personalMovieDB);
-console.log("hello");
-prompt('hello');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[b] = d;
+console.log(personalMovieDB);
